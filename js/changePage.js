b938d97fe0c9a5f004986901844286787e424f42
LIONORLIX <71428910+LIONORLIX@users.noreply.github.com>
2021-07-07 23:01:57 +0800

    Add files via upload

---
+++ js/changePage.js
@@ -0,0 +1,20 @@
+
+    function home(){
+    window.location.href="index.html";
+}
+    function coding(){
+    window.location.href="coding.html";
+}
+function graphics(){
+    window.location.href="graphics.html";
+}
+function me(){
+    window.location.href="aboutMe.html";
+}
+function article1(){
+    window.location.href="codingArticle1.html";
+}
+
+function article2(){
+    window.location.href="codingArticle2.html";
+}
